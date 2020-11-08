f47a7efc4a9ca52d66995019459c8c11409d7d0d
Noel2002 <berwanoel@gmail.com>
2020-11-08 12:37:01 +0200

    form validation

--- JS/createBlog.js
+++ JS/createBlog.js
@@ -2,15 +2,37 @@
 var form= document.getElementById('form');
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
-    db.collection('blogsTest').add({
-        title: form.title.value,
-        content: form.description.value,
-        writer: form.writer.value
-    });
-
-    form.title.value='';
-    form.description.value='';
-    form.writer.value='';
+    if(validate()){
+        db.collection('blogsTest').add({
+            title: form.title.value,
+            content: form.description.value,
+            writer: form.writer.value
+        });
+    
+        form.title.value='';
+        form.description.value='';
+        form.writer.value='';
+        alert('Blog created succesfully');
+        location.replace('../UI/view-blog.html');
+    }
+    
 })
 
+function validate(){
+    if(form.writer.value==""){
+        alert('Please Enter your name!');
+        return false;
+    }
+    if(form.title.value==""){
+        alert('Please enter the title');
+        return false;
+    }
+    if(form.description.value==""){
+        alert('Please enter the content of your blog!');
+        return false;
+    }
+    else{
+        return true;
+    }
+}
 
