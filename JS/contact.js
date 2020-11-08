f516aa73fc720593dd0634fa952c4a331c28252a
Noel2002 <berwanoel@gmail.com>
2020-11-08 12:58:06 +0200

    Replacing getDay() with getDate() and displaying Month in words

--- JS/contact.js
+++ JS/contact.js
@@ -3,7 +3,8 @@ var form= document.getElementById('form');
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
     var today= new Date();
-    var msg_date= today.getMonth()+' '+ today.getDay()+', '+ today.getFullYear()+'  '+ today.getHours()+':'+today.getMinutes();
+    var arrMonths= ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+    var msg_date= arrMonths[today.getMonth()] +' '+ today.getDate()+', '+ today.getFullYear()+'  '+ today.getHours()+':'+today.getMinutes();
     db.collection('messages').add({
         sender: form.username.value,
         content: form.message.value,
