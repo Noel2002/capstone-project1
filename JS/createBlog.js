88f3aa0f15d4d594c292b66ce2c43f78fac6657f
Noel2002 <berwanoel@gmail.com>
2020-11-09 12:36:24 +0200

    adding upload image feature

--- JS/createBlog.js
+++ JS/createBlog.js
@@ -1,19 +1,43 @@
+var url;
+function uploadImage(){
+    var image= document.getElementById('file-input').files[0];
+    var imageName= image.name;
+    var reference= firebase.storage().ref('images/'+imageName);
+    var uploadTask= reference.put(image);
+    
+    uploadTask.on('state_changed', function(snapshot){
+        var progress= (snapshot.bytesTransferred/ snapshot.totalBytes)*100;
+        console.log('upload is:' + progress + 'done');
+    }, function(error){
+        console.log(error.message);
+    }, function(){
+        uploadTask.snapshot.ref.getDownloadURL().then((downloadURL)=>{
+            url= downloadURL;
+            console.log(url);
+            alert('things are still good');
 
+            
+        });
+    });
+    
+}
 var form= document.getElementById('form');
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
+    uploadImage();
     if(validate()){
         db.collection('blogsTest').add({
             title: form.title.value,
             content: form.description.value,
-            writer: form.writer.value
+            writer: form.writer.value,
+            imgUrl: url
         });
     
         form.title.value='';
         form.description.value='';
         form.writer.value='';
         alert('Blog created succesfully');
-        location.replace('../UI/view-blog.html');
+        
     }
     
 })
