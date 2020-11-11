34e7739b2828e6dcb5ffbde4cf742e2d2ee807f4
Noel2002 <berwanoel@gmail.com>
2020-11-11 09:37:45 +0200

    allowing user to edit text fields of blog

--- JS/edit-blog.js
+++ JS/edit-blog.js
@@ -1,19 +1,32 @@
-
+let inputTitle= document.getElementById('title');
+let inputWriter= document.getElementById('writer');
+let inputContent= document.getElementById('content');
 var queryString = location.search.substring(1);
 console.log(queryString);
 
-db.collection('blogsTest').where('id', queryString).get().then((snapshot)=>{
-    snapshot.docs.forEach(doc => {
-        fillform(doc);
-    });
+db.collection('blogsTest').doc(queryString).get().then((snapshot)=>{
+    console.log(snapshot.data());
+    fillform(snapshot);
 });
 
 function fillform(doc){
-    let title= document.getElementById('title');
-    let writer= document.getElementById('writer');
-    let content= document.getElementById('content');
 
-    title.value= doc.title;
-    writer.value= doc.writer;
-    content.value= doc.content;
+    inputTitle.value= doc.data().title;
+    inputWriter.value= doc.data().writer;
+    inputContent.value= doc.data().content;
+    alert('data inserted successfully');
+
 }
+
+let updateBtn=document.getElementById('update-btn');
+updateBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    db.collection('blogsTest').doc(queryString).update({
+        title: inputTitle.value,
+        writer: inputWriter.value,
+        content: inputContent.value
+    });
+   
+    alert('data updated successfully');
+
+});
